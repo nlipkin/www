09dd659ad206928422bc2af2137857dea56390f5
nlipkin <nplipkin@gmail.com>
2016-07-06 00:35:15 +0300

    1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html data-page="construction">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Сайт в разработке</title>
+    </head>
+    <body>
+<?php
+include 'header.php';
+?>
+
+
+            <h1>Сайт в разработке</h1>
+            <p>Если вы являетесь администратором этого сайта, вам необходимо заменить эту страницу своими файлами.</p>
+        <div id="root"></div>
+    </body>
+</html>
